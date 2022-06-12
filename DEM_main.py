b8dd6afdc0e898d60b8a4318b85408f45a1dd0a4
Domojestic <domojestic@gmail.com>
2022-06-12 19:27:12 -0400

    Created the main module
    This module will be the 'executable,' so to speak, for the Dungeons Environment Manager. Its functionality hinges on other modules being created and implemented, and for the time being, this commit is simply to have the file exist in the repo.

---
+++ DEM_main.py
@@ -0,0 +1,7 @@
+"""
+Dungeons Environment Manager - main module
+
+This module will be the primary file run to use the DEM. It will call from other
+modules frequently, and I will try to keep it as simple as possible, delegating
+most of the heavy lifting to other modules.
+"""
