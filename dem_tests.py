0a82e75f08bb08d6bea7b27fd446aea8d2ab7644
Domojestic <marcialmachado0522@gmail.com>
2022-08-23 22:59:08 -0400

    Added dem_tests
    Okay, NOW I should be committing and pushing from the right account. Hopefully I get it right this time.

---
+++ dem_tests.py
@@ -0,0 +1,19 @@
+"""
+A testing module for the functions and methods made across the DEM repository.
+"""
+
+
+def test_char_in_comm_high_level() -> None:
+    """
+    Test that Character in Community returns True for a character being a
+    high-level member of that Community
+    """
+    pass
+
+
+def test_char_in_comm_low_level() -> None:
+    """
+    Test that Character in Community returns True for a character being a
+    low-level member of that Community
+    """
+    pass
